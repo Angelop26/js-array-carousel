b3749eb8d41b47494b9a1241516f7c9d586a55e7
Angelop26 <angeloprospero26@gmail.com>
2023-03-14 15:16:25 +0100

    milestone 3 complete

--- js/script.js
+++ js/script.js
@@ -6,7 +6,39 @@ const listItem = document.querySelector('.list-item')
 for (let i = 0; i < imgArray.length; i++) {    
     const currentElem = imgArray[i]
     listItem.innerHTML +=` 
-        <div class="item active">
+        <div class="item">
             <img src="${currentElem}" alt="">
         </div>`
 }
+
+// partenza
+const itemArray = document.getElementsByClassName('item')
+let itemArrayIndex = 0
+itemArray[itemArrayIndex].classList.add('active')
+
+// Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+// implementiamo la freccia in alto 
+const upbtn = document.querySelector('.up-slide')
+upbtn.addEventListener('click', function() {
+    // togliere active da quello corrente
+    itemArray[itemArrayIndex].classList.remove('active')
+
+    // incremento l'index
+    itemArrayIndex++;
+
+    // aggiungere active a quello successivo
+    itemArray[itemArrayIndex].classList.add('active')
+
+})
+
+const dwnbtn = document.querySelector('.down-slide');
+dwnbtn.addEventListener('click', function(){
+       // togliere active da quello corrente
+       itemArray[itemArrayIndex].classList.remove('active')
+
+       // decremento l'index
+       itemArrayIndex--;
+   
+       // aggiungere active a quello precedente
+       itemArray[itemArrayIndex].classList.add('active')
+})
